5ac3c90ac85af26c0190f1a2a15d7440d4f22136
Wuzhiyin <34057023+Wuzhiyin@users.noreply.github.com>
2019-07-03 21:04:05 +0800

    完整代码提交

--- src/cn/heima/day22/test/Test1.java
+++ src/cn/heima/day22/test/Test1.java
@@ -2,7 +2,6 @@ package cn.heima.day22.test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Test1 {
@@ -12,7 +11,7 @@ public class Test1 {
 	 * 定义一个文件输入流,调用read(byte[] b)方法,将a.txt文件中的内容打印出来(byte数组大小限制为5)
 	 *
 	 * 分析:
-	 * 1,reda(byte[] b)是字节输入流的方法,创建FileInputStream,关联a.txt
+	 * 1,read(byte[] b)是字节输入流的方法,创建FileInputStream,关联a.txt
 	 * 2,创建内存输出流,将读到的数据写到内存输出流中
 	 * 3,创建字节数组,长度为5
 	 * 4,将内存输出流的数据全部转换为字符串打印
