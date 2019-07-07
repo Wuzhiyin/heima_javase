b001c8df098e5feea9300b1390c2b5db46f7751e
Wuzhiyin <34057023+Wuzhiyin@users.noreply.github.com>
2019-07-07 18:39:30 +0800

    完整代码提交

--- src/cn/heima/day01/Hello2.java
+++ src/cn/heima/day01/Hello2.java
@@ -13,6 +13,7 @@ class Hello2 {									//这是一个类的声明,类名是Hello2	//单行注释
     public static void main(String[] args) {	//这是主方法,是程序的入口
         System.out.println("Hello World!");		//这是输出语句,会在控制台输出一句话
         System.out.println("Hello World!");
+
     }
 
 	/*
